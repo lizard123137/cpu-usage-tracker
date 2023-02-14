a30e93af4ab35574c84e26f1aa9ee6e8b476e293
Michal Gagos <lizard123137@gmail.com>
2023-02-14 10:46:29 +0100

    removed some redundant code

--- reader.c
+++ reader.c
@@ -1,35 +1,5 @@
 #include "reader.h"
 
-static int get_cpu_data(CPU_DATA *readbuffer_data) {
-    FILE *file = fopen("/proc/stat", "r");
-
-    char data[1024];
-
-    if (file == NULL)
-        return -1;
-
-    for(int idx = 0; idx < onlineProcessorsAmount; idx++) {
-        fgets(data, sizeof(data), file);
-
-        sscanf(data, "cpu%d %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu",  
-            &readbuffer_data[idx].cpu,
-            &readbuffer_data[idx].user,
-            &readbuffer_data[idx].nice,
-            &readbuffer_data[idx].system,
-            &readbuffer_data[idx].idle,
-            &readbuffer_data[idx].iowait,
-            &readbuffer_data[idx].irq,
-            &readbuffer_data[idx].softirq,
-            &readbuffer_data[idx].steal,
-            &readbuffer_data[idx].guest,
-            &readbuffer_data[idx].guest_nice);
-    }
-
-    if (fclose(file) != 0)
-        return -1;
-    return 0;
-}
-
 void *reader(void *arg) {
     (void)arg;
 
@@ -37,7 +7,32 @@ void *reader(void *arg) {
         sem_wait(&readSemaphoreFull);
         pthread_mutex_lock(&readBufferMutex);
 
-        if (get_cpu_data(get_readbuffer_data()) == -1)
+        FILE *file = fopen("/proc/stat", "r");
+        CPU_DATA *readbuffer_data = get_readbuffer_data();
+
+        char data[1024];
+
+        if (file == NULL)
+            exit(EXIT_FAILURE);
+
+        for(int idx = 0; idx < onlineProcessorsAmount; idx++) {
+            fgets(data, sizeof(data), file);
+
+            sscanf(data, "cpu%d %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu",  
+                &readbuffer_data[idx].cpu,
+                &readbuffer_data[idx].user,
+                &readbuffer_data[idx].nice,
+                &readbuffer_data[idx].system,
+                &readbuffer_data[idx].idle,
+                &readbuffer_data[idx].iowait,
+                &readbuffer_data[idx].irq,
+                &readbuffer_data[idx].softirq,
+                &readbuffer_data[idx].steal,
+                &readbuffer_data[idx].guest,
+                &readbuffer_data[idx].guest_nice);
+        }
+
+        if (fclose(file) != 0)
             exit(EXIT_FAILURE);
         
         pthread_mutex_unlock(&readBufferMutex);
